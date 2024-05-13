5d695a223179075f016184869af62f9c9710c2d9
morsalin-islam335 <weshallovercome335@gmail.com>
2024-05-13 22:43:35 +0600

    convert the temparature

---
+++ convert_the_temparature.py
@@ -0,0 +1,9 @@
+
+# problem link:  https://leetcode.com/problems/convert-the-temperature/
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        ans = []
+        ans.append(celsius + 273.15)
+        ans.append((celsius * 1.80) + 32.00)
+        return ans
+        
