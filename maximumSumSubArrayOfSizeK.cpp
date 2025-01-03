003285f84839e960ac313de8d2bb247bd8985ead
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-01-03 18:20:01 +0600

    maximum sum subarray of size k leetcode solution

---
+++ maximumSumSubArrayOfSizeK.cpp
@@ -0,0 +1,33 @@
+// problem link: https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long ans = 0;
+        long long currentSum = 0;
+        int begin = 0;
+        int end = 0;
+
+        unordered_map<int, int> numToIndex;
+
+        while (end < nums.size()) {
+            int currNum = nums[end];
+            int lastOccurrence =
+                (numToIndex.count(currNum) ? numToIndex[currNum] : -1);
+
+            // if current window already has number or if window is too big,
+            // adjust window
+            while (begin <= lastOccurrence || end - begin + 1 > k) {
+                currentSum -= nums[begin];
+                begin++;
+            }
+            numToIndex[currNum] = end;
+            currentSum += nums[end];
+            if (end - begin + 1 == k) {
+                ans = max(ans, currentSum);
+            }
+            end++;
+        }
+        return ans;
+    }
+};
